db6ae1d7744d68121bc0f00fbc2d84bb2a796341
caelchristian <caelchristian23@gmail.com>
2022-04-19 18:35:16 -0400

    Moved functions to Game. Last commit didn't work

--- sfml.cpp
+++ sfml.cpp
@@ -3,27 +3,9 @@
 
 int main()
 {
-
-    Game game();
-
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+    Game game;
+    while (game.isWindowOpen() && !(game.hasGameEnded())) {
+        
     }
+    return 0;
 }
