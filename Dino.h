601a656ca7ccbe643aac4bcbd733f91dbe7a7996
caelchristian <caelchristian23@gmail.com>
2022-04-20 18:29:49 -0400

    Added Dino class which player will control

---
+++ Dino.h
@@ -0,0 +1,22 @@
+#ifndef M4OEP_CCHRIST7_QWARDEN_DINO_H
+#define M4OEP_CCHRIST7_QWARDEN_DINO_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include "SFML/Graphics.hpp"
+using namespace sf;
+
+enum ColorDino{
+    green,yellow,red,blue
+};
+
+class Dino {
+private:
+    float speed;
+    ColorDino playerColor;
+    Texture DinoTexture;
+public:
+    Dino()
+};
+
+#endif //M4OEP_CCHRIST7_QWARDEN_DINO_H
