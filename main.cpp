83b435e4787b860abdf9d8223b8a10ac8d5c1b2e
qwarden <quimbywarden@gmail.com>
2022-04-24 23:44:31 -0400

    even better hitbox

--- main.cpp
+++ main.cpp
@@ -271,7 +271,7 @@ int main()
                 // move obstacles down path
                 obs.move(-OBSMOVESPEED, 0);
                 FloatRect bounds = obs.getGlobalBounds();
-                FloatRect smaller(bounds.left + 25, bounds.top, bounds.width - 40, bounds.height - 30);
+                FloatRect smaller(bounds.left + 25, bounds.top, bounds.width - 50, bounds.height - 30);
 
                 // if dino overlaps with obstacles
                 if (dino.getGlobalBounds().intersects(smaller))
